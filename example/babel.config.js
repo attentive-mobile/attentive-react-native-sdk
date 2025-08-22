8dc1a9749b62f62f58d8692a00c7337cbd2f85bb
Sergio Daniel <me@zheref.io>
2025-08-22 15:15:39 -0500

    Migrate main RN codebase along with Android

--- example/babel.config.js
+++ example/babel.config.js
@@ -1,12 +1,3 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-        alias: {},
-      },
-    ],
-  ],
+  presets: ['module:@react-native/babel-preset'],
 };
