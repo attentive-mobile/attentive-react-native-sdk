7e839f13e7b9739951002937cb7c2a48fb3434ce
Sergio Lozano <slozani@attentivemobile.com>
2026-03-09 23:17:19 -0500

    Adjustments

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,28 +1,28 @@
-import React, { useEffect } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React, { useEffect } from 'react'
+import { NavigationContainer } from '@react-navigation/native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import {
   initialize,
   identify,
   type AttentiveSdkConfiguration,
   type UserIdentifiers,
-} from '@attentive-mobile/attentive-react-native-sdk';
+} from '@attentive-mobile/attentive-react-native-sdk'
 
-import HomeScreen from './HomeScreen';
-import ProductScreen from './ProductScreen';
-import type { RootStackParamList } from './navTypes';
+import HomeScreen from './HomeScreen'
+import ProductScreen from './ProductScreen'
+import type { RootStackParamList } from './navTypes'
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+const Stack = createNativeStackNavigator<RootStackParamList>()
 
 function App(): React.JSX.Element {
   useEffect(() => {
     // Initialize the SDK with New Architecture support
     const config: AttentiveSdkConfiguration = {
       attentiveDomain: 'vs',
-      mode: 'debug',
+      mode: 'verbose',
       enableDebugger: true,
-    };
-    initialize(config);
+    }
+    initialize(config)
 
     // Identify the user
     const identifiers: UserIdentifiers = {
@@ -32,9 +32,9 @@ function App(): React.JSX.Element {
       shopifyId: 'userShopifyId',
       clientUserId: 'userClientUserId',
       customIdentifiers: { customIdKey: 'customIdValue' },
-    };
-    identify(identifiers);
-  }, []);
+    }
+    identify(identifiers)
+  }, [])
 
   return (
     <NavigationContainer>
